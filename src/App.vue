38cc199ff588af833dc546bcb92cb4d4ed05ad92
zaien24 <zaien24@gmail.com>
2020-10-14 23:14:42 +0900

    136~139 [컴포넌트등록]

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
+import TodoHeader form './components/TodoHeader.vue'
+import TodoInput form './components/TodoInput.vue'
+import TodoList form './components/TodoList.vue'
+import TodoFooter form './components/TodoFooter.vue'
+
 export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
 }
 </script>
 
