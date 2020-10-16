b16e8f4bb0eaaf7cb29a717422dc38963ad953ac
zaien24 <zaien24@gmail.com>
2020-10-16 22:53:02 +0900

    150~151
    [컴포넌트 내용 구현하기]
     # 저장된 할 일 목록을 표시하는 TodoList 컴포넌트
       - 할 일 목록 만들기
       - 로컬 스토리지 데이터를 뷰 데이터에 저장하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul></ul>
+            <li>할일 1</li>
+            <li>할일 2</li>
+            <li>할일 3</li>
+    </section>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    created() {
+        if(localStorage.length > 0) {
+            for(var i = 0; i < localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
 }
 </script>
 
