665be3e1acce2825189d2b65407086b0b1f62192
zaien24 <zaien24@gmail.com>
2020-10-16 23:03:26 +0900

    152~154
    [컴포넌트 내용 구현하기]
     # 저장된 할 일 목록을 표시하는 TodoList 컴포넌트
       - 뷰 데이터의 아이템 개수만큼 화면에 표시하기
    
     # TodoList.vue 에 할 일 삭제 기능 추가하기
       - 할 일 목록 & 삭제 버튼 마크업 작업하기
       - 할 일 삭제 버튼에 클릭 이벤트 추가하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,14 @@
 <template>
     <section>
-        <ul></ul>
-            <li>할일 1</li>
-            <li>할일 2</li>
-            <li>할일 3</li>
+        <ul>
+            <li v-for="todoItem in todoItems" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{todoItem}}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
     </section>
 </template>
 
@@ -16,13 +21,59 @@ export default {
     },
     created() {
         if(localStorage.length > 0) {
-            for(var i = 0; i < localStorage.length; i++) {
+            for (var i = 0; i < localStorage.length; i++) {
                 this.todoItems.push(localStorage.key(i));
             }
+        }
+    },
+    methods: {
+        removeTodo() {
+            console.log("clicked");
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+
+
+  .list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .list-move {
+    transition: transform 1s;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
 </style>
